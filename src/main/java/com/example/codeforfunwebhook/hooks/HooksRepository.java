a75325b6f10a1e35cf456967c916dc4aed702b9f
大胃王 <253498229@qq.com>
2018-07-05 15:33:47 +0800

    polish

--- src/main/java/com/example/codeforfunwebhook/hooks/HooksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.codeforfunwebhook.hooks;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * @author wangbin
- */
-public interface HooksRepository extends JpaRepository<Hooks, String> {
-}
