a4e87955937f12af88a401bd99bafe35e9b3b061
大胃王 <253498229@qq.com>
2018-07-05 16:48:10 +0800

    polish

--- src/test/java/com/example/codeforfunwebhook/CodeforfunWebhookApplicationTests.java
+++ src/test/java/com/example/codeforfunwebhook/CodeforfunWebhookApplicationTests.java
@@ -1,30 +1,12 @@
 package com.example.codeforfunwebhook;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.util.UUID;
 
 public class CodeforfunWebhookApplicationTests {
-  private OkHttpClient client = new OkHttpClient();
-
   @Test
-  public void contextLoads() throws IOException {
-    String url = "http://jenkins.codeforfun.cn/job/codeforfun-webhooks/build?token=jfdlaskjfljwerijoqweruq";
-    String s = get(url);
-    System.out.println(s);
-
-
-  }
-
-  public String get(String url) throws IOException {
-    Request request = new Request.Builder()
-            .url(url)
-            .build();
-
-    Response response = client.newCall(request).execute();
-    return response.body().string();
+  public void test(){
+    System.out.println(UUID.randomUUID());
   }
 }
