1b03432df41ac35d43d57d33328855e34f091ff4
大胃王 <253498229@qq.com>
2018-07-05 15:42:34 +0800

    polish

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 cd /root/codeforfun/codeforfun-webhooks
+rm -rf target
+cp /root/codeforfun/codeforfun-jenkins/data/workspace/codeforfun-webhooks/target .
 docker-compose build
 docker-compose rm -s -f
 docker-compose up -d
